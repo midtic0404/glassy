35c5b8b687d73158c69f5885356a26055a5d8aed
Chia Che Chang <midtic0404@gmail.com>
2020-03-13 23:14:51 -0600

    Remove commented code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,9 +20,6 @@
 	<h1 class="is-size-3">Select An Ingredient</h1>
 	<div class="columns is-multiline is-centered">
 		{#each ingredients as ingredient, index (index)}
-			<!-- <NoteCard 
-			description="{note}"
-			on:delete={deleteNote(index)} /> -->
 			<div class="column">
 				<a href='drinks/{ingredient}' class="button is-medium is-link is-light">{ ingredient} </a>
 			</div>
